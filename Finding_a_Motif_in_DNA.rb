07b6059dfae8a14d307b432d6128c103e78a141c
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2015-02-13 11:40:48 -0500

    Finished solver script

--- Finding_a_Motif_in_DNA.rb
+++ Finding_a_Motif_in_DNA.rb
@@ -15,19 +15,13 @@ dna_strings = content.split(/\r?\n/)
 dna_string =  dna_strings[0]
 dna_substring =  dna_strings[1]
 
-puts dna_string
-puts dna_substring
-
 indexes = Array.new
 j = dna_string.index(dna_substring, 0)
-#puts j
 
 while j != nil
   indexes << j+1 if j != nil
   j = dna_string.index( dna_substring, j+1 )
-  #puts j if j != nil
 end
 
-puts indexes.inspect
 indexes.each { |x| print x.to_s + " " }
 puts "\n"
